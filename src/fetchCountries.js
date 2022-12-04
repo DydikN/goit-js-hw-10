f37717b92ca4012df83a2e195641702573250d32
DydikN <PingvinPain@gmail.com>
2022-12-04 16:38:02 +0200

    add some styles

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
-export function fetchCountries(name) {
-  const URL = `https://restcountries.com`;
-  const fields = `fields=name,capital,population,flags,languages`;
+const URL = `https://restcountries.com`;
+const fields = `fields=name,capital,population,flags,languages`;
 
+export function fetchCountries(name) {
   return fetch(`${URL}/v3.1/name/${name}?${fields}`).then(resp => {
     if (!resp.ok) {
       return Promise.reject(new Error());
